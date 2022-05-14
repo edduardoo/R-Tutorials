0016d8e2d8a3f58a76159083ec8927a5aed71b9c
Eduardo Jardim <duhjardim@hotmail.com>
2022-05-13 23:21:09 -0300

    tutorial completo

---
+++ Template Monografia DSBD UFPR/packages_installation.R
@@ -0,0 +1,8 @@
+# package that facilitates writing books and long-form articles/reports with R Markdown
+install.packages("bookdown") # https://bookdown.org/
+
+# LaTeX distribution based on TeX Live:
+install.packages("tinytex") # https://yihui.org/tinytex/
+
+# Helps to build common complex tables and manipulate table styles. 
+install.packages("kableExtra") # https://cran.r-project.org/web/packages/kableExtra/vignettes/awesome_table_in_html.html
